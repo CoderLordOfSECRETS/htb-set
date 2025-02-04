2ba14cfb78686d1a32dce39c85c82f9c62e8426e
CoderLordOfSECRETS <118375520+CoderLordOfSECRETS@users.noreply.github.com>
2025-02-04 15:12:04 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 pip install pyftpdlib
 
 echo '#!/bin/bash
-pyftpdlib --certfile <(openssl req -x509 -newkey rsa:2048 -keyout /tmp/tempftps.key -out /tmp/tempftps.crt -days 1 -nodes -subj "/CN=localhost") "$@"' | sudo tee /usr/local/bin/tempftps > /dev/null && sudo chmod +x /usr/local/bin/tempftps
+python3 -m pyftpdlib --certfile <(openssl req -x509 -newkey rsa:2048 -keyout /tmp/tempftps.key -out /tmp/tempftps.crt -days 1 -nodes -subj "/CN=localhost") "$@"' | sudo tee /usr/local/bin/tempftps > /dev/null && sudo chmod +x /usr/local/bin/tempftps
 echo "created tempftps"
 
